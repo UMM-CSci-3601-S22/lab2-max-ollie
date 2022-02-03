13868c82f8ebf2da310a640f047d227968553f2e
Oliver Willette <wille667@morris.umn.edu>
2022-02-03 16:23:25 -0600

    Actually add the 3 files
    Co-authored-by: Quintmax <quint218@umn.edu>

--- server/src/main/java/umm3601/todos/todosDatabase.java
+++ server/src/main/java/umm3601/todos/todosDatabase.java
@@ -14,16 +14,58 @@ import io.javalin.http.BadRequestResponse;
 /**
  * A fake "database" of todos
  */
-public class todosDatabase{
-	private todos[] allTodos;
-	public todosDatabase(String todoData) throws StreamReadException, DatabindException, IOException{
+public class todosDatabase {
+  private todo[] allTodos;
+
+  public todosDatabase(String todoData) throws StreamReadException, DatabindException, IOException {
     InputStreamReader reader = new InputStreamReader(getClass().getResourceAsStream(todoData));
     ObjectMapper objectMapper = new ObjectMapper();
-    allTodos = objectMapper.readValue(reader, todos[].class);
-	}
+    allTodos = objectMapper.readValue(reader, todo[].class);
+  }
 
   public int size(){
     return allTodos.length;
   }
-  public todos
+
+  public todo getTodo(String id) {
+    return Arrays.stream(allTodos).filter(x -> x._id.equals(id)).findFirst().orElse(null);
+  }
+
+  public todo[] listTodos(Map<String, List<String>> queryParams) {
+    todo filteredTodos[] = allTodos;
+
+    // filter by status if defined
+    if (queryParams.containsKey("status")) {
+      String statusParam = queryParams.get("status").get(0);
+      try {
+        Boolean targetStatus = Boolean.parseBoolean(statusParam);
+        filteredTodos = filterTodosByStatus(filteredTodos, targetStatus);
+      } catch (NumberFormatException e) {
+        throw new BadRequestResponse("Specified status '" + statusParam + "' can't be parsed to a bool");
+      }
+    }
+    if (queryParams.containsKey("owner")) {
+      String targetOwner = queryParams.get("owner").get(0);
+      filteredTodos = filterTodosByOwner(filteredTodos, targetOwner);
+    }
+    if (queryParams.containsKey("category")) {
+      String targetCategory = queryParams.get("category").get(0);
+      filteredTodos = filterTodosByCategory(filteredTodos, targetCategory);
+    }
+    return filteredTodos;
+  }
+
+  private todo[] filterTodosByStatus(todo[] todos, Boolean targetStatus) {
+    return Arrays.stream(todos).filter(x -> x.status == targetStatus).toArray(todo[]::new);
+  }
+
+  public todo[] filterTodosByOwner(todo[] todos, String targetOwner) {
+    return Arrays.stream(todos).filter(x -> x.owner.equals(targetOwner)).toArray(todo[]::new);
+  }
+
+  public todo[] filterTodosByCategory(todo[] todos, String targetCategory) {
+    return Arrays.stream(todos).filter(x -> x.category.equals(targetCategory)).toArray(todo[]::new);
+  }
+
+
 }
